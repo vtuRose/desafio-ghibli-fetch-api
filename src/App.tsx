0fd62594493af880f5a36d5a1aa379fa125c1df6
Vitor R <vitorferraz12r@gmail.com>
2026-09-17 04:48:18 -0300

    feat: Altera BrowserRouter para HashRouter na configuração de rotas para funcionar no github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { FilmList } from "./pages/FilmList";
 import { FilmDetails } from "./pages/FilmDetails";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<FilmList />} />
           <Route path="/films/:id" element={<FilmDetails />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
